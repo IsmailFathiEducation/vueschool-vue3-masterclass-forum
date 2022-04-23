5b9780d19fa5b44bb6a69425f24b00d59dab3c15
ismail-fathi <ismailhtfathi@gmail.com>
2022-04-23 18:19:52 +0000

    feat: added the standard eslint preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,23 @@
 module.exports = {
-        "root": true,
-        "env": {
-                "node": true
-        },
-        "extends": [
-                "plugin:vue/vue3-essential",
-                "eslint:recommended"
-        ],
-        "parserOptions": {
-                "parser": "@babel/eslint-parser"
-        },
-        "rules": {
-                "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn"
-        }
+  root: true,
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/standard'
+  ],
+
+  env: {
+    node: true
+  },
+
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn'
+  }
 }
