14d14e6efcaafb211d0a29119fdcce8e61b79320
ismail-fathi <ismailhtfathi@gmail.com>
2022-04-23 22:12:49 +0000

    fix: removed some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn'
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'space-before-function-paren': 'off'
   }
 }
